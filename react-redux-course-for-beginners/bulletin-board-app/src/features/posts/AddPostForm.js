21d91ff17ef8938144e4421953be36684bd1bf82
wagner <wggauer@gmail.com>
2023-06-30 19:12:30 -0300

    create AddPostForm component and import postAdded reducer to create new posts

---
+++ react-redux-course-for-beginners/bulletin-board-app/src/features/posts/AddPostForm.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+
+import { postAdded } from "./postsSlice";
+
+const AddPostForm = () => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+
+  const onSavePostClicked = (e) => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      );
+    }
+  };
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent"> content:</label>
+        <textarea
+          id="postContent"
+          name="postConent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button onClick={onSavePostClicked} type="button">
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
